23f64c6ee12d335f80317e9a48c3b03a0d7adf55
Thiago Silva <adsl.thiago@gmail.com>
2025-08-22 17:51:46 -0300

    Feature: Deploy Automático

---
+++ .scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Entrar no modo Manutenção
+(php artisan down) || true
+
+
+#Atualizando Branch
+git pull origin main
+
+#instalar Composer
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+
+#optimize
+php artisan optimize
+
+#compilar os assets
+#nvm
+
+export NVM_DIR="~/.nvm"
+source ~/.nvm/nvm.sh
+
+npm install --yes
+
+npm run build
+
+
+#migrations
+php artisan migrate --force
+
+
+#sair do modo manutenção
+php artisan up
