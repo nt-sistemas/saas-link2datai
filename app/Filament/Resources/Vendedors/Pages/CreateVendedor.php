89eb556e0ceb09b282c24b033dc0ada740c00553
Thiago Silva <adsl.thiago@gmail.com>
2025-09-19 13:17:45 -0300

    Fix: Ajustes Vendedores

--- app/Filament/Resources/Vendedors/Pages/CreateVendedor.php
+++ app/Filament/Resources/Vendedors/Pages/CreateVendedor.php
@@ -3,9 +3,32 @@
 namespace App\Filament\Resources\Vendedors\Pages;
 
 use App\Filament\Resources\Vendedors\VendedorResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use PhpParser\Node\Expr\Throw_;
 
 class CreateVendedor extends CreateRecord
 {
     protected static string $resource = VendedorResource::class;
+    protected static bool $canCreateAnother = false;
+
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $vendedorExists = VendedorResource::getModel()::where('document', strtoupper($data['document']))
+            ->where('tenant_id', auth()->user()->tenant_id)
+            ->exists();
+
+
+        $data['tenant_id'] = auth()->user()
+            ->tenant_id;
+        $data['name'] = strtoupper($data['name']);
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/Vendedors/Schemas/VendedorForm.php
+++ app/Filament/Resources/Vendedors/Schemas/VendedorForm.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\Vendedors\Schemas;
 
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Component;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 
 class VendedorForm
@@ -13,8 +15,21 @@ class VendedorForm
         return $schema
             ->components([
                 TextInput::make('document')
-                    ->required(),
+                    ->label('CPF')
+                    ->required()
+                    ->rule(static function (Get $get, Component $component): \Closure {
+                        return function ($attribute, $value, $fail) use ($get, $component) {
+                            $vendedorExists = \App\Models\Vendedor::where('document', strtoupper($value))
+                                ->where('tenant_id', auth()->user()->tenant_id)
+                                ->exists();
+
+                            if ($vendedorExists) {
+                                $fail('Este CPF já está cadastrado.');
+                            }
+                        };
+                    }),
                 TextInput::make('name')
+                    ->label('Nome')
                     ->required(),
                 Select::make('filial_id')
                     ->label('Filial')
@@ -25,4 +40,6 @@ class VendedorForm
                     ->preload(),
             ]);
     }
+
+
 }
