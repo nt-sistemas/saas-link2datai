6818abad10f82ed593518e9bf1c3c6b1903888b0
Thiago Silva <adsl.thiago@gmail.com>
2025-10-15 18:39:50 -0300

    feature:Telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\AuthServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
