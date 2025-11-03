d36ce0f90c1ce30f3994ee1f9f03333359816cfc
Thiago Silva <adsl.thiago@gmail.com>
2025-11-03 19:26:49 -0300

    Ajuste Format

--- app/Jobs/ProcessImportJob.php
+++ app/Jobs/ProcessImportJob.php
@@ -50,7 +50,7 @@ class ProcessImportJob implements ShouldQueue
             $dateString = $row->data['Data Pedido'];
 
             // The format of the input string
-            $format = 'd/m/Y H:i:s';
+            $format = 'd/m/Y H:i';
             $carbonDate = Carbon::createFromFormat($format, $dateString);
 
 
