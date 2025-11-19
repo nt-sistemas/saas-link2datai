72453dc1ead5c7d3af9e5356cdfca718d40e6c08
Thiago Silva <adsl.thiago@gmail.com>
2025-11-19 12:02:16 -0300

    Fix:Graficos Gráfico Mensal

--- resources/views/livewire/categories/show.blade.php
+++ resources/views/livewire/categories/show.blade.php
@@ -53,6 +53,22 @@
     @endif
 
     {{-- The best athlete wants his opponent at his best. --}}
+    <div class="flex flex-col gap-4 justify-between">
+        <div class="p-4 rounded-lg shadow-md bg-white">
+            <span class="font-semibold">Total de Vendas Mensal - {{ $group->name }}</span>
+            <livewire:app.charts.chart-bar-valor-mensal :grupo_id="$group->id" :dt_inicio="$data_ini"
+                                                        :dt_fim="$data_fim"
+                                                        :filiais_multi_ids="$filiais_multi_ids"/>
+        </div>
+
+        <div class="p-4 bg-white rounded-lg shadow-md">
+            <span class="font-semibold">Quantidade de Vendas Mensal - {{ $group->name }}</span>
+            <livewire:app.charts.chart-bar-quantidade-mensal :grupo_id="$group->id" :dt_inicio="$data_ini"
+                                                             :dt_fim="$data_fim"
+                                                             :filiais_multi_ids="$filiais_multi_ids"
+            />
+        </div>
+    </div>
     <div class="flex flex-col gap-4 justify-between h-full">
         <div class="p-4 rounded-lg shadow-md bg-white">
             <span class="font-semibold">Total de Vendas - {{ $group->name }}</span>
