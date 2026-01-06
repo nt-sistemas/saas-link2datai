462f7752134a5bbb7af7ead1178a257ee58d1380
Thiago Silva <adsl.thiago@gmail.com>
2026-01-06 17:56:18 -0300

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Permission;
-use App\Models\Tenant;
 use App\Models\User;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,47 +15,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        Permission::insert([
-            [
-                'id' => Str::uuid()->toString(),
-                'key' => 'be an admin'
-            ],
-            [
-                'id' => Str::uuid()->toString(),
-                'key' => 'be a manager'
-            ],
-            [
-                'id' => Str::uuid()->toString(),
-                'key' => 'be a user'
-            ],
-
-        ]);
-
-        $tenant = Tenant::factory()->create([
-            'name' => 'Link2B Telecom',
-            'slug' => 'link2b',
-            'phone' => '(11) 99999-9999',
-            'email' => 'imagem@link2b.com.br',
-            'active' => true,
-        ]);
-
-
-        $user = User::factory()->create([
+        User::factory()->create([
             'name' => 'Link2B',
             'email' => 'admin@link2b.com.br',
             'password' => bcrypt('password'),
+            'is_admin' => true,
+            'tenant_id' => null,
         ]);
-
-        $userManager = User::create([
-            'name' => 'Imagem Telecom',
-            'email' => 'imagem@link2b.com.br',
-            'password' => bcrypt('password'),
-            'tenant_id' => $tenant->id,
-        ]);
-        $userManager->givePermissionTo('be a manager');
-
-        $user->givePermissionTo('be an admin');
-
 
     }
 }
