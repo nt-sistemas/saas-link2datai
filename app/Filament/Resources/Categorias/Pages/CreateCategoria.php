7e41eb312465984aaed5092e425f726ff392f169
Thiago Silva <adsl.thiago@gmail.com>
2026-01-08 13:32:17 -0300

    Correção

--- app/Filament/Resources/Categorias/Pages/CreateCategoria.php
+++ app/Filament/Resources/Categorias/Pages/CreateCategoria.php
@@ -9,6 +9,14 @@ class CreateCategoria extends CreateRecord
 {
     protected static string $resource = CategoriaResource::class;
 
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+
+        $data['tenant_id'] = auth()->user()->tenant_id;
+
+        return $data;
+    }
+
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
--- app/Filament/Resources/Grupos/Pages/CreateGrupo.php
+++ app/Filament/Resources/Grupos/Pages/CreateGrupo.php
@@ -8,4 +8,12 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateGrupo extends CreateRecord
 {
     protected static string $resource = GrupoResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+
+        $data['tenant_id'] = auth()->user()->tenant_id;
+
+        return $data;
+    }
 }
